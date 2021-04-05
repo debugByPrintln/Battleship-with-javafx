577f980ee2d2c14c9786dd3cea656b10a12a4b39
nikita <melnikov.n.89@mail.ru>
2021-04-06 02:33:52 +0300

    changed some stuff just to check if it works

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 600, 600);
+        scene = new Scene(loadFXML("primary"), 700, 700);
         stage.setScene(scene);
         stage.show();
     }
